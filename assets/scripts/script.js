8e3f2209004d6f3b1ec55ba85ae3ff97faa03a2f
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-08-05 20:50:28 -0600

    added prompt for password length input

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,7 +4,13 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
     console.log("generatePassword is running");
-    
+    var passwordLength = prompt("How many characters?")
+        if (passwordLength < 8) {
+            alert("Too few characters selected. Please click \"generate password\" please try again");
+        }
+        else if (passwordLength > 128) {
+            alert("Too many characters. Please click \"generate password\" to try again.");
+        }
 }
 
 
