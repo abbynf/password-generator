150b254df8f139dd85ff95bed3c45c594cec84f4
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-08-05 20:57:36 -0600

    added confirm for option to include numbers in password

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,12 +7,15 @@ function generatePassword() {
     var passwordLength = prompt("How many characters?");
         if (passwordLength < 8) {
             alert("Too few characters selected. Please click \"generate password\" please try again");
+            return;
         }
         else if (passwordLength > 128) {
             alert("Too many characters. Please click \"generate password\" to try again.");
+            return;
         }
     var lowerCase = confirm("Would you like lower case letters in the password?");
     var upperCase = confirm("Would you like capital letters in the password?")
+    var numbers = confirm("Would you like numbers in the password?")
 }
 
 
