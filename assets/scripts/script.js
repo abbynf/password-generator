25eed51c8968e4a475a38ff817e182051be97a67
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-08-05 23:05:59 -0600

    added for loop to add characters of password together into an array

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ""];
-var upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ""];
-var numbersOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ""];
+var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbersOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specCharOptions = ["@", "%", "!", "#", "$", "?", "&", "*"];
 
 function generatePassword() {
@@ -47,11 +47,11 @@ function generatePassword() {
 
     console.log(options);
 
-    // for (i = 0; i < passwordLength; i++) {
-    //     var start = "";
-    //     start += options[Math.floor(Math.random() * options.length)];
-    //     console.log(start);
-    // }
+    for (i = 0; i < passwordLength; i++) {
+        var start = "";
+        start += options[Math.floor(Math.random() * options.length)];
+        console.log(start);
+    }
 
 
 
