7bd3719f2f1e89e2e4543379befb89d09133e847
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-08-05 22:13:06 -0600

    added arrays to be concated if user inputs confirm as true

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbersOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ""];
+var upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ""];
+var numbersOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ""]
 var specCharOptions = ["@", "%", "!", "#", "$", "?", "&", "*"]
 
 
@@ -22,6 +22,27 @@ function generatePassword() {
     var upperCase = confirm("Would you like capital letters in the password?");
     var numbers = confirm("Would you like numbers in the password?");
     var specialChar = confirm("Would you like special characters in the password?");
+
+    var options = ""
+
+    if (lowerCase == true)  {
+        var options = options.concat(lowerCaseOptions);
+        }
+
+    if (upperCase == true) {
+        var options = options.concat(upperCaseOptions);
+    }
+
+    if (numbers == true) {
+        var options = options.concat(numbersOptions);
+    }
+
+    if (specialChar == true) {
+        var options = options.concat(specCharOptions);
+    }
+
+    console.log(options);
+
 
 }
 
