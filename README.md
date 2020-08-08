bfc0a5eff7d6ffd42e7e8a2cbbc121af0db4d6f3
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-08-08 15:43:00 -0600

    added readme file describing application

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # password-generator
+
+## Description
+
+This password generator allows the user to create a series of random characters that may be used as a password. A password is most secure when it is comprised of random characters instead of key words or phrases from a user's life, and this password-generator makes the process of picking a password very simple.
+
+
+![screenshot of landing page](./assets/images/screenshot.png)
+
+
+## How to use this application
+1. Click on the red "Generate Password" button.
+2. Choose the number of characters you want your password to have. You may choose as few as 8 and as many as 128. Click 'OK' to submit.
+3. Choose what kind of characters you would like your password to have. Click 'OK' for yes and 'cancel' for no. 
+4. The generated password will appear above the red button. 
+
+
+Access this application at https://abbynf.github.io/password-generator/
+
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,15 @@
-// Assignment Code
+// Code for the generate variable
 var generateBtn = document.querySelector("#generate");
 
+// Variables for each of the character options, which may be combined based on user input
 var lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbersOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specCharOptions = ["@", "%", "!", "#", "$", "?", "&", "*"];
 
 function generatePassword() {
-    console.log("generatePassword is running");
     var passwordLength = prompt("How many characters?");
+        // if/else conditional to ensure a minimum of 8 characters and a maximum of 128
         if (passwordLength < 8) {
             alert("Too few characters selected. Please click \"generate password\" please try again");
             return;
@@ -17,61 +18,50 @@ function generatePassword() {
             alert("Too many characters. Please click \"generate password\" to try again.");
             return;
         }
+
+    // List of variables that allow the user to choose character types using confirms
     var lowerCase = confirm("Would you like lower case letters in the password?");
     var upperCase = confirm("Would you like capital letters in the password?");
     var numbers = confirm("Would you like numbers in the password?");
-    var specialChar = confirm("Would you like special characters in the password?");
+    var specialChar = confirm("Would you like special characters in the password? i.e. @ % ! # $ ? & *");
 
+    // if conditional to ensure that the user selects at least one type of character
     if (lowerCase == false && upperCase == false && numbers == false && specialChar == false){
         alert("No characters chosen. Please click \"generate password\" to try again");
     }
 
    
+    // This array will be used to collect all of the character options. It starts out empty. The password will be chosen from this array
     var options = [""]
 
+    // a series of if conditions will add the arrays from before if the user opts for that category of character
     if (lowerCase == true)  {
         var options = options.concat(lowerCaseOptions);
         }
-
     if (upperCase == true) {
         var options = options.concat(upperCaseOptions);
     }
-
     if (numbers == true) {
         var options = options.concat(numbersOptions);
     }
-
     if (specialChar == true) {
         var options = options.concat(specCharOptions);
     }   
 
+    // The options array started with a blank string that must be removed so it doesn't get chosen as a character
     options.shift()
 
-    // set up an empty array from the characters to go into
+    // set up an empty array from the characters for the password to go into after the character has been chosen from the options array
     var draft = [];
-
-        var yen = 10;
 
     // this for loop inserts random characters from the options array into the empty array
     for (i = 0; i < passwordLength; i++){
         draft.push(options[Math.floor(Math.random()*options.length)]);
     }
 
-
-
+    // The draft array is joined to change it from an array to a string with no commas or quotation marks
   return draft.join("");
-
-
-    // The issue where you left off: the above for loop prints out the random characters according to the input criteria, but 
-    // it prints out the characters one at the time into separate lines. 
-    // I'm now trying to figure out how to print them out into one single array instead of separate strings 
-    // what if i had a function that popped out a random index number and put it into an array? then had a loop that repeated the fnction X number of times? 
-
-    // what if after creating the options array from the input, we did this:
-    // popped the first indeex out X number of times, where X is the lenght of the password chosen? 
-
 }
-
 
 
 // Write password to the #password input
